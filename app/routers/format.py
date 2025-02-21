3cd37f627b9dfc144ada0c459b08d3e5ba72ac5e
Dylan Navajas Gluck <dylan.n.gluck@gmail.com>
2025-02-21 01:48:44 -0500

    Feat: Adds /format and /parse enpoint logic

--- app/routers/format.py
+++ app/routers/format.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
-from ..types import Resume
+from google import genai
+from vlmrun.hub.schemas.document.resume import Resume
 
 router = APIRouter()
+client = genai.Client()
 
 @router.put("/format")
 async def format_resume(resume: Resume, job_description: str):
@@ -9,24 +11,31 @@ async def format_resume(resume: Resume, job_description: str):
     Formats a resume based on the provided job description.
 
     Args:
-        resume (Resume): The resume to be formatted.
-        job_description (str): The job description to be used for formatting.
+        resume (Resume): JSON object representing the resume to be formatted.
+        job_description (string): The job description to be used for formatting.
 
     Returns:
-        dict: The formatted resume.
+        status (HTTPStatus): The status of the formatting operation.
+        data (Resume): The formatted resume object.
     """
-    formatted_resume = {
-        "name": "",
-        "email": "",
-        "phone": "",
-        "address": "",
-        "links": [],
-        "skills": [],
-        "work_experience": [],
-        "projects": [],
-        "certifications": [],
-        "education": [],
-        "languages": [],
-        "interests": []
-    }
-    return formatted_resume
+
+    # LLMs love XML :)
+    resume_xml = f"<resume>{resume.json()}</resume>"
+    job_description_xml = f"<job_description>{job_description}</job_description>"
+
+    prompt = "Refine the resume to best match the attached job description"
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        config={
+            "response_mime_type": "application/json",
+            "response_schema": Resume,
+        },
+        contents=[
+            resume_xml,
+            job_description_xml,
+            prompt,
+        ],
+    )
+
+    parsed_data = Resume.model_validate(response.parsed)
+    return {"status": 200, "data": parsed_data}
--- app/routers/parse.py
+++ app/routers/parse.py
@@ -1,30 +1,51 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile, File
+from google import genai
+from google.genai import types
+from vlmrun.hub.schemas.document.resume import Resume
 
 router = APIRouter()
+client = genai.Client()
+
 
 @router.put("/parse")
-async def parse_resume(resume):
+async def parse_resume(file: UploadFile = File(...)):
     """
-        Process resume document and return a dictionary of the parsed data.
+    Process resume document and return a dictionary of the parsed data.
 
-        Args:
-            resume: File object (pdf, docx, txt, json, xml)
+    Args:
+        file: File object (pdf, md, txt)
 
-        Returns:
-            dict: JSON dictionary of parsed data
+    Returns:
+        status: HTTPStatus code
+        data: JSON dictionary of parsed Resume
     """
-    parsed_data = {
-        "name": "",
-        "email": "",
-        "phone": "",
-        "address": "",
-        "links": [],
-        "skills": [],
-        "work_experience": [],
-        "projects": [],
-        "certifications": [],
-        "education": [],
-        "languages": [],
-        "interests": []
-    }
-    return parsed_data
+
+    if file.content_type == "application/pdf":
+        pass
+    elif file.content_type == "text/markdown":
+        pass
+    elif file.content_type == "text/plain":
+        pass
+    else:
+        raise ValueError("Unsupported file type")
+
+    content = await file.read()
+
+    prompt = "Extract all available resume information"
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        config={
+            "response_mime_type": "application/json",
+            "response_schema": Resume,
+        },
+        contents=[
+            types.Part.from_bytes(
+                data=content,
+                mime_type=file.content_type,
+            ),
+            prompt,
+        ],
+    )
+
+    parsed_data = Resume.model_validate(response.parsed)
+    return {"status": 200, "data": parsed_data}
